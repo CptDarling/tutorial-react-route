f1f4a729340f46f731a39573f704c4f442a19bf1
Richard Holyoak <rholyoak@gmail.com>
2022-12-22 11:16:03 +0000

    Updating Data

--- src/main.jsx
+++ src/main.jsx
@@ -13,6 +13,7 @@ import ErrorPage, { } from "./error-page";
 import Contact, {
   loader as contactLoader,
 } from "./routes/contact";
+import EditContact from './routes/edit';
 
 const router = createBrowserRouter([
   {
@@ -21,14 +22,18 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     loader: rootLoader,
     action: rootAction,
-    children:
-      [
-        {
-          path: "contacts/:contactId",
-          element: <Contact />,
-          loader: contactLoader,
-        },
-      ],
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+        loader: contactLoader,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: contactLoader,
+      },
+    ],
   }
 ]);
 
