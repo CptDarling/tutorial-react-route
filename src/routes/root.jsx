CptDarling/tutorial-react-route

commit be5ffdff24674271560bffed3b60e98f2412fbad

Active Link Styling

src/routes/root.jsx
@@ -1,79 +1,91 @@
 import {
-    Link,
-    Outlet,
-    useLoaderData,
-    Form,
-    redirect,
+  Link,
+  Outlet,
+  NavLink,
+  useLoaderData,
+  Form,
+  redirect,
 } from "react-router-dom";
 import { getContacts, createContact } from "../contacts";
 
 export async function action() {
-    const contact = await createContact();
-    return redirect(`/contacts/${contact.id}/edit`);
+  const contact = await createContact();
+  return redirect(`/contacts/${contact.id}/edit`);
 }
 
 export async function loader() {
-    const contacts = await getContacts();
-    return { contacts };
+  const contacts = await getContacts();
+  return { contacts };
 }
 
 export default function Root() {
-    const { contacts } = useLoaderData();
-    return (
-        <>
-            <div id="sidebar">
-                <h1>React Router Contacts</h1>
-                <div>
-                    <form id="search-form" role="search">
-                        <input
-                            id="q"
-                            aria-label="Search contacts"
-                            placeholder="Search"
-                            type="search"
-                            name="q"
-                        />
-                        <div
-                            id="search-spinner"
-                            aria-hidden
-                            hidden={true}
-                        />
-                        <div
-                            className="sr-only"
-                            aria-live="polite"
-                        ></div>
-                    </form>
-                    <Form method="post">
-                        <button type="submit">New</button>
-                    </Form>
-                </div>
-                <nav>
-                    {contacts.length ? (
-                        <ul>
-                            {contacts.map((contact) => (
-                                <li key={contact.id}>
-                                    <Link to={`contacts/${contact.id}`}>
-                                        {contact.first || contact.last ? (
-                                            <>
-                                                {contact.first} {contact.last}
-                                            </>
-                                        ) : (
-                                            <i>No Name</i>
-                                        )}{" "}
-                                        {contact.favorite && <span>★</span>}
-                                    </Link>
-                                </li>
-                            ))}
-                        </ul>
-                    ) : (
-                        <p>
-                            <i>No contacts</i>
-                        </p>
-                    )}
-                </nav>
-            </div>
-            <div id="detail">
-                <Outlet />
-            </div>
-        </>
-    );
+  const { contacts } = useLoaderData();
+  return (
+    <>
+      <div id="sidebar">
+        <h1>React Router Contacts</h1>
+        <div>
+          <form id="search-form" role="search">
+            <input
+              id="q"
+              aria-label="Search contacts"
+              placeholder="Search"
+              type="search"
+              name="q"
+            />
+            <div
+              id="search-spinner"
+              aria-hidden
+              hidden={true}
+            />
+            <div
+              className="sr-only"
+              aria-live="polite"
+            ></div>
+          </form>
+          <Form method="post">
+            <button type="submit">New</button>
+          </Form>
+        </div>
+        <nav>
+          {contacts.length ? (
+            <ul>
+              {contacts.map((contact) => (
+                <li key={contact.id}>
+                  <NavLink
+                    to={`contacts/${contact.id}`}
+                    className={({ isActive, isPending }) =>
+                      isActive
+                        ? "active"
+                        : isPending
+                          ? "pending"
+                          : ""
+                    }
+                  >
+                    <Link to={`contacts/${contact.id}`}>
+                      {contact.first || contact.last ? (
+                        <>
+                          {contact.first} {contact.last}
+                        </>
+                      ) : (
+                        <i>No Name</i>
+                      )}{" "}
+                      {contact.favorite && <span>★</span>}
+                    </Link>
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>
+              <i>No contacts</i>
+            </p>
+          )}
+        </nav>
+      </div>
+      <div id="detail">
+        <Outlet />
+      </div>
+    </>
+  );
 }

src/routes/edit.jsx
@@ -2,10 +2,10 @@ import {
   Form,
   useLoaderData,
   redirect,
- } from "react-router-dom";
+} from "react-router-dom";
 import { updateContact } from "../contacts";
 
-export async function action({request, params}) {
+export async function action({ request, params }) {
   const formData = await request.formData();
   const updates = Object.fromEntries(formData);
   console.log(updates);
@@ -20,14 +20,14 @@ export default function EditContact() {
     <Form method="post" id="contact-form">
       <p>
         <span>Name</span>
-        <input 
+        <input
           placeholder="First"
           aria-label="First name"
           type="text"
           name="first"
           defaultValue={contact.first}
         />
-        <input 
+        <input
           placeholder="Last"
           aria-label="Last name"
           type="text"
@@ -37,7 +37,7 @@ export default function EditContact() {
       </p>
       <label>
         <span>Twitter</span>
-        <input 
+        <input
           type="text"
           name="twitter"
           placeholder="@jack"
@@ -56,7 +56,7 @@ export default function EditContact() {
       </label>
       <label>
         <span>Notes</span>
-        <textarea 
+        <textarea
           name="notes"
           defaultValue={contact.notes}
           rows={6}

commit 1f21e5f8f3c05e1a60c7d2ca53c646216110bb9e

Fix NavLink error
Bug introduced by be5ffdf: Active Link Styling

src/routes/root.jsx
@@ -69,7 +69,6 @@ export default function Root() {
                           : ""
                     }
                   >
-                    <Link to={`contacts/${contact.id}`}>
                       {contact.first || contact.last ? (
                         <>
                           {contact.first} {contact.last}
@@ -78,7 +77,6 @@ export default function Root() {
                         <i>No Name</i>
                       )}{" "}
                       {contact.favorite && <span>★</span>}
-                    </Link>
                   </NavLink>
                 </li>
               ))}