425e1c3b5d77b0bfef178f3f0294b6bc65c414b7
Richard Holyoak <rholyoak@gmail.com>
2023-01-12 17:48:07 +0000

    Submitting Forms onChange

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,13 +1,13 @@
 import { useEffect } from "react";
 
 import {
-  Link,
   Outlet,
   NavLink,
   useLoaderData,
   Form,
   redirect,
   useNavigation,
+  useSubmit,
 } from "react-router-dom";
 
 import { 
@@ -30,6 +30,7 @@ export async function loader( {request} ) {
 export default function Root() {
   const { contacts, q } = useLoaderData();
   const navigation = useNavigation();
+  const submit = useSubmit();
 
   useEffect(() => {
       document.getElementById("q").value = q;
@@ -48,6 +49,9 @@ export default function Root() {
               type="search"
               name="q"
               defaultValue={q}
+              onChange={(event) => {
+                submit(event.currentTarget.form);
+              }}
             />
             <div
               id="search-spinner"
