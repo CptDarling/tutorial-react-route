c31f021466f966c24f422587794f359053228510
Richard Holyoak <rholyoak@gmail.com>
2023-01-16 16:32:56 +0000

    JSX Routes

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {
+  createRoutesFromElements,
   createBrowserRouter,
   RouterProvider,
+  Route,
 } from "react-router-dom";
 
 import './index.css';
@@ -28,41 +30,37 @@ import { action as destroyAction } from './routes/destroy';
 
 import Index from "./routes/index";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <ErrorPage />,
-    loader: rootLoader,
-    action: rootAction,
-    children: [
-      {
-        errorElement: <ErrorPage />,
-        children: [
-          { index: true, element: <Index /> },
-          {
-            path: "contacts/:contactId",
-            element: <Contact />,
-            loader: contactLoader,
-            action: contactAction,
-          },
-          {
-            path: "contacts/:contactId/edit",
-            element: <EditContact />,
-            loader: contactLoader,
-            action: editAction,
-          },
-          {
-            path: "contacts/:contactId/destroy",
-            action: destroyAction,
-            errorElement: <div>Oops! There was an error.</div>,
-            // errorElement: <ErrorPage />,
-          },
-        ],
-      }
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route
+      path="/"
+      element={<Root />}
+      loader={rootLoader}
+      action={rootAction}
+      errorElement={<ErrorPage />}
+    >
+      <Route errorElement={<ErrorPage />} >
+        <Route index element={<Index />} />
+        <Route
+          path='contacts/:contactId'
+          element={<Contact />}
+          loader={contactLoader}
+          action={contactAction}
+        />
+        <Route
+          path='contacts/:contactId/edit'
+          element={<EditContact />}
+          loader={contactLoader}
+          action={editAction}
+        />
+        <Route
+          path='contacts/:contactId/destroy'
+          action={destroyAction}
+        />
+      </Route>
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
